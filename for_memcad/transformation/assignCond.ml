5bd15fe5829a82e1b6232b5674b238368b5f6684
Francois BERENGER <francois.berenger@inria.fr>
2015-12-02 17:14:40 +0100

    transform_assign_expr became transform_test_expr and can now handle logic binops in addition to the already handled relat. binops

--- for_memcad/transformation/assignCond.ml
+++ for_memcad/transformation/assignCond.ml
@@ -20,22 +20,26 @@ open Tests
 module L = BatList
 
 (* (ptr == 0) --> ((ptr == 0) ? 1 : 0) *)
-let rec transform_assign_expr negate clang e2 =
+let rec transform_test_expr negate clang e2 =
   match Tests.get_unary_not e2 with (* !(ptr == 0) *)
-  | `Unary_not e -> transform_assign_expr true clang e
+  | `Unary_not e -> transform_test_expr true clang e
   | `None ->
+    let int_1 = Codegen.integer_literal_expr clang e2.e_sloc 1 in
+    let int_0 = Codegen.integer_literal_expr clang e2.e_sloc 0 in
     match Tests.get_relat_or_equal_binop e2 with
-    | `None -> None
     | `Relat_or_equal_binop (_op, _e3, _e4) -> (* (ptr == 0) *)
-      let int_1 = Codegen.integer_literal_expr clang e2.e_sloc 1 in
-      let int_0 = Codegen.integer_literal_expr clang e2.e_sloc 0 in
-      let new_e2 =
-        if negate then
-          { e2 with e = ConditionalOperator (e2, int_0, int_1) }
-        else
-          { e2 with e = ConditionalOperator (e2, int_1, int_0) }
-      in
-      Some new_e2
+      Some { e2 with e = (if negate
+                          then ConditionalOperator (e2, int_0, int_1)
+                          else ConditionalOperator (e2, int_1, int_0)) }
+    | `None ->
+      match Tests.get_logic_binop e2 with
+      | `LAnd (e1, e2) ->  (* (e1 && e2) --> ((e1) ? e2 : 0) *)
+        assert(negate = false); (* not handled *)
+        Some { e2 with e = ConditionalOperator (e1, e2, int_0) }
+      | `LOr (e1, e2) -> (* (e1 || e2) --> ((e1) ? 1 : e2) *)
+        assert(negate = false); (* not handled *)
+        Some { e2 with e = ConditionalOperator (e1, int_1, e2) }
+      | `None -> None
 
 let transform_decl (clang: Clang.Api.clang) =
   let rec map_stmt
@@ -44,11 +48,28 @@ let transform_decl (clang: Clang.Api.clang) =
       (stmt: stmt): (stmt list) * stmt =
     match stmt.s with
     | CompoundStmt stmts -> MapStmt.mapCompoundStmt map_stmt v state stmt stmts
+    | ReturnStmt (Some e) ->
+      begin match transform_test_expr false clang e with
+        | None -> MapVisitor.visit_stmt v state stmt (* default *)
+        | Some e' ->
+          let tmp_var = Codegen.fresh_varname "assignCond" in
+          let sloc = e.e_sloc in
+          let tloc = Types.tloc_of_ctyp sloc (Codegen.int_type clang) in
+          let decl_tmp_var =
+            Codegen.declare_stmt ~init:e' sloc tmp_var tloc
+          in
+          let new_ret_val = Some { e with e = DeclRefExpr tmp_var } in
+          let new_return_stmt = { stmt with s = ReturnStmt new_ret_val } in
+          let new_stmt =
+            Codegen.compound_stmt decl_tmp_var new_return_stmt sloc
+          in
+          map_stmt v state new_stmt
+      end
     | ExprStmt e ->
       begin match Tests.get_assign e with
         | `None -> MapVisitor.visit_stmt v state stmt (* default *)
         | `Assign (e1, e2) -> (* int i = (ptr == 0); *)
-          begin match transform_assign_expr false clang e2 with
+          begin match transform_test_expr false clang e2 with
             | None -> MapVisitor.visit_stmt v state stmt (* default *)
             | Some new_e2 ->
               let new_e = { e with e = BinaryOperator (BO_Assign, e1, new_e2) } in
@@ -57,7 +78,7 @@ let transform_decl (clang: Clang.Api.clang) =
           end
       end
     | DeclStmt [{ d = VarDecl (ty, name, Some init) } as decl] ->
-      begin match transform_assign_expr false clang init with
+      begin match transform_test_expr false clang init with
         | None -> MapVisitor.visit_stmt v state stmt (* default *)
         | Some new_init ->
           let new_decl = { decl with d = VarDecl (ty, name, Some new_init) } in
